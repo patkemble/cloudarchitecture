f772b48f50e34d7f512d51d48fb7c5f7bba9e23e
pkemble <patrick.kemble@davita.com>
2017-05-15 07:14:46 -0600

    added authorization service

---
+++ authorization-service/src/main/java/com/davita/cloud/AuthorizationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.davita.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableAuthorizationServer
+@EnableResourceServer
+@SpringBootApplication
+public class AuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServiceApplication.class, args);
+	}
+}
