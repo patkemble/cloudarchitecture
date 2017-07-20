0213e514e8699ce2088254f3781ea750ecc88553
Pat Kemble <pat_kemble@yahoo.com>
2017-07-20 17:04:25 -0600

    updates to package names and pom files

--- authorization-service/src/test/java/com/pk/cloud/AuthorizationServiceApplicationTests.java
+++ authorization-service/src/test/java/com/pk/cloud/AuthorizationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.davita.cloud;
+package com.pk.cloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- discovery-service/src/main/java/com/pk/cloud/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/pk/cloud/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.davita.cloud;
+package com.pk.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
